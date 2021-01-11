f3d23e8201cd279849e0494ef599c51adb46712f
EvgenyShvyrkov <zhenya100694@mail.ru>
2021-01-11 18:07:22 +0300

    Add immutable class and tests

--- src/main/java/com/andersen/Person.java
+++ src/main/java/com/andersen/Person.java
@@ -1,4 +1,29 @@
 package com.andersen;
 
-public class Person {
+public final class Person {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public final String showPersonInfo(){
+        return String.format("first name: %s\nlast name: %s\nage: %d", firstName, lastName, age);
+    }
 }
--- src/test/java/com/andersen/PersonTest.java
+++ src/test/java/com/andersen/PersonTest.java
@@ -1,12 +1,34 @@
 package com.andersen;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+    private static final String personInfo = "first name: Evgeny\nlast name: Ivanov\nage: 26";
+    private Person person;
+    private Person secondPerson;
+
+    @BeforeEach
+    void init() {
+        person = new Person("Evgeny", "Ivanov", 26);
+        secondPerson = new Person("Evgeny", "Ivanov", 26);
+    }
 
     @Test
-    void showPersonInfo() {
+    void shouldReturnPersonsInfo() {
+        assertEquals(person.showPersonInfo(), personInfo);
+    }
+
+    @Test
+    void shouldCompareDifferentLinksAndReturnTheyAreNotEqual() {
+        assertNotSame(person, secondPerson);
+    }
+
+    @Test
+    void shouldCompareSameLinksAndReturnTheyAreEqual() {
+        secondPerson = person;
+        assertSame(person, secondPerson);
     }
 }
