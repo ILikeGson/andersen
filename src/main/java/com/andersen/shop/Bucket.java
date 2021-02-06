f9b0b490d34a4eb56a1c74583be085bf59a1575e
ZhenyaShvyrkov <67383878+ZhenyaShvyrkov@users.noreply.github.com>
2021-02-06 12:18:45 +0300

    Delete src/main/java/com/andersen/shop directory
    delete

--- src/main/java/com/andersen/shop/Bucket.java
+++
@@ -1,9 +0,0 @@
-package com.andersen.shop;
-
-public interface Bucket {
-    void showProducts();
-    void addProduct(Product product);
-    void removeById(int id);
-    void clear();
-    double price();
-}
--- src/main/java/com/andersen/shop/Shop.java
+++
@@ -1,9 +0,0 @@
-package com.andersen.shop;
-
-public interface Shop {
-    void showAllProducts();
-    void showProductsInBucket(int userId);
-    void addProductToBucketById(int id, int userId);
-    void removeFromBucketById(int id, int userId);
-    void clear(int userId);
-}
